7fe66bbabfb635af57e540db8d50a73368ebffa0
yacaFx <sergiobritor@gmail.com>
2021-05-08 14:33:53 -0400

    feat(session-1): add order library

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/restaurant', '<rootDir>/libs/dc-ui'],
+  projects: [
+    '<rootDir>/apps/restaurant',
+    '<rootDir>/libs/dc-ui',
+    '<rootDir>/libs/order',
+  ],
 };
