437324457c4546c0aff67dd271d66163d30485a3
yacaFx <sergiobritor@gmail.com>
2021-05-22 14:31:14 -0400

    feat(session-3): add material animations

--- apps/restaurant/src/app/app.module.ts
+++ apps/restaurant/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Route, RouterModule } from '@angular/router';
 import { AuthModule, authRoutes } from '@dc/auth';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ const routes: Route[] = [{ path: 'auth', children: authRoutes }];
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
     AuthModule
   ],
