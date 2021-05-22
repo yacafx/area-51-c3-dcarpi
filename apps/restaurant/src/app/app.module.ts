498e900453475238a801c05a177d26a36f4d5540
yacaFx <sergiobritor@gmail.com>
2021-05-22 12:14:55 -0400

    feat(session-3): add auth route

--- apps/restaurant/src/app/app.module.ts
+++ apps/restaurant/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Route, RouterModule } from '@angular/router';
+import { AuthModule, authRoutes } from '@dc/auth';
 import { AppComponent } from './app.component';
+
+const routes: Route[] = [{ path: 'auth', children: authRoutes }];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+    AuthModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,16 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
-import { LoginComponent } from './containers/login/login.component';
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { LoginComponent } from './containers/login/login.component';
 
-export const authRoutes: Route[] = [];
+export const authRoutes: Route[] = [
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [
-    LoginComponent,
-    LoginFormComponent
-  ]
+  declarations: [LoginComponent, LoginFormComponent]
 })
 export class AuthModule {}
