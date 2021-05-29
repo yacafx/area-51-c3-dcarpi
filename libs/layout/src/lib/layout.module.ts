d33ea791921fcbb7178ed0e5f64cef0c751a1c59
yacaFx <sergiobritor@gmail.com>
2021-05-29 11:17:46 -0400

    feat(session-4): add material and  router modules

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MaterialModule } from '@dc/material';
 import { LayoutComponent } from './containers/layout/layout.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   declarations: [LayoutComponent]
 })
 export class LayoutModule {}
