4fcb6547f3723408130dd905265001cd745e6477
yacaFx <sergiobritor@gmail.com>
2021-05-29 12:56:26 -0400

    feat(session-4): consume user auth observable

--- libs/layout/src/lib/containers/layout/layout.component.ts
+++ libs/layout/src/lib/containers/layout/layout.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '@dc/auth';
 
 @Component({
   selector: 'dc-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LayoutComponent {
+  constructor(public authService: AuthService) {}
 }
