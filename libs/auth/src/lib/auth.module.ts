8e22c13b91467a05a88505cce4bd46b873df80f1
yacaFx <sergiobritor@gmail.com>
2021-05-29 10:46:59 -0400

    feat(session-4): add reactive forms

--- libs/auth/src/lib/auth.module.ts
+++ libs/auth/src/lib/auth.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { Route, RouterModule } from '@angular/router';
 import { MaterialModule } from '@dc/material';
 import { LoginFormComponent } from './components/login-form/login-form.component';
@@ -11,7 +12,13 @@ export const authRoutes: Route[] = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule, HttpClientModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
   declarations: [LoginComponent, LoginFormComponent]
 })
 export class AuthModule {}
