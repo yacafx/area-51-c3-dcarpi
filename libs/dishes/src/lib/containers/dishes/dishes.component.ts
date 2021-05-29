157b4ab3611dbd9c08e20a498a8d3eafb30ab664
yacaFx <sergiobritor@gmail.com>
2021-05-29 14:17:47 -0400

    feat(session-4): add dishes base component

---
+++ libs/dishes/src/lib/containers/dishes/dishes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dc-dishes',
+  templateUrl: './dishes.component.html',
+  styleUrls: ['./dishes.component.scss']
+})
+export class DishesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/dishes/src/lib/dishes.module.ts
+++ libs/dishes/src/lib/dishes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { DishesComponent } from './containers/dishes/dishes.component';
 
 @NgModule({
   imports: [
@@ -8,6 +9,9 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ])
+  ],
+  declarations: [
+    DishesComponent
   ]
 })
 export class DishesModule {}
