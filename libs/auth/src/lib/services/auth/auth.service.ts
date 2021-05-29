6955bc18e275eadbc84b59350647c168d5f5f231
yacaFx <sergiobritor@gmail.com>
2021-05-29 12:55:12 -0400

    feat(session-4): expose user auth data

--- libs/auth/src/lib/services/auth/auth.service.ts
+++ libs/auth/src/lib/services/auth/auth.service.ts
@@ -1,17 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Authenticate, User } from '@dc/models';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private url = 'http://localhost:3000';
+  private userSubject$ = new BehaviorSubject<User>({} as User);
+  user$ = this.userSubject$.asObservable();
 
   constructor(private http: HttpClient) {}
 
   login(auth: Authenticate): Observable<User> {
-    return this.http.post<User>(`${this.url}/login`, auth);
+    return this.http
+      .post<User>(`${this.url}/login`, auth)
+      .pipe(tap((user) => this.userSubject$.next(user)));
   }
 }
