c11d5f0fcc7937b13d361ba02b9626117a8e3236
yacaFx <sergiobritor@gmail.com>
2021-05-29 11:57:15 -0400

    feat(session-4): unsubscribe after 1 time
    Signed-off-by: yacaFx <sergiobritor@gmail.com>

--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Authenticate } from '@dc/models';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { take, takeUntil } from 'rxjs/operators';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
@@ -10,17 +10,10 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent implements OnDestroy {
-  private unsubscribe$: Subject<void> = new Subject();
-
+export class LoginComponent {
   constructor(private auth: AuthService) {}
 
   login(auth: Authenticate): void {
-    this.auth.login(auth).pipe(takeUntil(this.unsubscribe$)).subscribe();
-  }
-
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
+    this.auth.login(auth).pipe(take(1)).subscribe();
   }
 }
