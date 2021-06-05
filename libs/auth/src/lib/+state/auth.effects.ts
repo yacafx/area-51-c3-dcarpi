87ececeb81a591829fa87d82bdfdae24a527bd35
yacaFx <sergiobritor@gmail.com>
2021-06-05 13:50:50 -0400

    feat(session-5): update base effect

--- libs/auth/src/lib/+state/auth.effects.ts
+++ libs/auth/src/lib/+state/auth.effects.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
-import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { User } from '@dc/models';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { fetch } from '@nrwl/angular';
-
-import * as AuthFeature from './auth.reducer';
+import { AuthService } from '../services/auth/auth.service';
 import * as AuthActions from './auth.actions';
 
 @Injectable()
 export class AuthEffects {
   init$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(AuthActions.init),
+      ofType(AuthActions.login),
       fetch({
         run: (action) => {
           // Your custom service 'load' logic goes here. For now just return a success action...
-          return AuthActions.loadAuthSuccess({ auth: [] });
+
+          const user = {} as User;
+          return AuthActions.loginSuccess({ payload: user });
         },
 
         onError: (action, error) => {
           console.error('Error', error);
-          return AuthActions.loadAuthFailure({ error });
+          return AuthActions.loginFailure({ payload: error });
         }
       })
     )
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private auth: AuthService) {}
 }
