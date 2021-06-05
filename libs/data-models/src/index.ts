469b39a413ca81ed648f63d268d0db68c7667944
yacaFx <sergiobritor@gmail.com>
2021-06-05 15:05:17 -0400

    feat(session-5): add dishes interfaces

--- libs/data-models/src/index.ts
+++ libs/data-models/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/interfaces/auth.interfaces';
+export * from './lib/interfaces/dish.interfaces';
+export * from './lib/interfaces/state.interfaces';
 export * from './lib/interfaces/user.interfaces';
 export * from './lib/types/common.types';
-export * from './lib/interfaces/state.interfaces';
---
+++ libs/data-models/src/lib/interfaces/dish.interfaces.ts
@@ -0,0 +1,7 @@
+export interface Dish {
+  id: number;
+  name: string;
+  photo?: string;
+  price: string;
+  available?: boolean;
+}
